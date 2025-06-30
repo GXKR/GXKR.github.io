4303772af8e723ef292db1e1aba6d9d9ca5f242f
The Gentleman <geoffwhite767@gmail.com>
2025-06-29 22:39:45 -0700

    Delete js/main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Mobile nav toggle
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-navToggle.addEventListener('click', ()=> {
-  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-});
